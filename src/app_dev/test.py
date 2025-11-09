b4c82b912fbce704a9a0669731bee4670150d36d
Keita Masui <rfrnndam.tennis365@gmail.com>
2025-11-09 14:16:06 +0000

    Delete python files

--- src/app_dev/test.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-import sys
-
-message = f"Hi it is {datetime.now()}.\r\nThis Python is {sys.version}."
-print(message)
--- src/data_engineering/data_scraping/jepx_scraper.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
-r = requests.get(url, timeout=10)
-data = r.json()
